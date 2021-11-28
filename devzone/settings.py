31eebfa20c33346843224fb839592ba98a929418
Minzhi Qu <quminzhi@gmail.com>
2021-11-28 11:16:05 -0800

    Added Greeting Mail

--- devzone/settings.py
+++ devzone/settings.py
@@ -142,3 +142,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email Settings
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'minzhq@gmail.com'
+EMAIL_HOST_PASSWORD = 'hesccljpklazodgp'
--- users/signals.py
+++ users/signals.py
@@ -2,7 +2,10 @@ from django.contrib.auth.models import User
 from .models import Profile
 
 from django.db.models.signals import post_save, post_delete
-from django.dispatch import receiver
+# from django.dispatch import receiver
+
+from django.core.mail import send_mail
+from django.conf import settings
 
 # # signal method 1:
 # @receiver(post_save, sender=Profile)
@@ -16,11 +19,22 @@ from django.dispatch import receiver
 def createProfile(sender, instance, created, **kwargs):
     if created:
         user = instance
-        Profile.objects.create(
+        profile = Profile.objects.create(
             user=user,
             username=user.username,
             email=user.email,
             name=user.first_name,
+        )
+        
+        subject = 'Welcome to UW DevZone'
+        message = 'We are glad you are here.'
+                
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently=False,
         )
 
 # if profile is update then update responding items in user
